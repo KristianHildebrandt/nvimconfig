0497b9e7cfff4a14038ed979e80c53b20d16f8ef
Kristian 'Kdawg' Hildebrandt <kristian.hildebrandt@kloeckner.com>
2025-10-30 19:11:04 +0100

    snippets working

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,33 +1,57 @@
 return {
-  -- Main completion engine
+  -- Core cmp + sources
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+  { "saadparwaiz1/cmp_luasnip" },
+  { "L3MON4D3/LuaSnip",        build = "make install_jsregexp" },
+  { "onsails/lspkind.nvim" },
+
   {
     "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
-      "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
       "onsails/lspkind.nvim",
     },
     config = function()
       local cmp = require("cmp")
       local lspkind = require("lspkind")
-
+      local luasnip = require("luasnip")
 
       cmp.setup({
         snippet = {
           expand = function(args)
-            require("luasnip").lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
           end,
         },
         mapping = cmp.mapping.preset.insert({
           ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
+          ["<CR>"]      = cmp.mapping.confirm({ select = true }),
+
+          -- Tab-aware of both cmp and LuaSnip (intended flow)
+          ["<Tab>"]     = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"]   = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
             else
               fallback()
             end
@@ -35,6 +59,7 @@ return {
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
+          { name = "codeium" }, -- Windsurf source
           { name = "luasnip" },
           { name = "buffer" },
           { name = "path" },
@@ -46,6 +71,20 @@ return {
             ellipsis_char = "...",
           }),
         },
+      })
+
+      -- Optional: cmdline completion (keep if you like it)
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "path" },
+          { name = "cmdline" },
+        },
+      })
+
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = { { name = "buffer" } },
       })
     end,
   },
